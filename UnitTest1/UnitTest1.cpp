5cdd9c82d0e9dfdce522bde9e8526bd02cfcd0bf
1827009 <43544451+1827009@users.noreply.github.com>
2020-05-24 23:36:42 +0900

    テスト

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Assert::IsTrue(!is_prime_number(1));
+			Assert::IsTrue(!is_prime_number(2));
+			Assert::IsTrue(!is_prime_number(3));
+			Assert::IsTrue(!is_prime_number(5));
+			Assert::IsTrue(!is_prime_number(7));
+		}
+	};
+}
